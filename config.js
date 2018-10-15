b9953d86641f86ae8ebb3d96c9468f48c9cb57c9
Hamza <lelouch.hamza@gmail.com>
2018-10-15 20:07:14 +0100

    Add webpack configuration for js scripts

--- config.js
+++ config.js
@@ -63,5 +63,25 @@ module.exports = {
   revManifest: {
     dest: 'app/',
     path: 'app/rev-manifest.json'
+  },
+  webpack: {
+    output: {
+      filename: '[name].js'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
   }
 };
